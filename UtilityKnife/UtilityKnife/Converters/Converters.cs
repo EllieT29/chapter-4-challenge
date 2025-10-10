28cf1eeea772ba4c4cedb1da8c594403f4cd15c7
Ellie Thomas <elliethom294@gmail.com>
2025-10-10 13:45:43 +0100

    Create skeleton for FtoC converter

---
+++ UtilityKnife/UtilityKnife/Converters/Converters.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilityKnife.Converters
+{
+    public class FahrenheitToCelsius
+    {
+        public double FahrenheitToCCelsiusConverter(double FahrenheitTemp)
+        {
+            double _fahrenheitTemp = 0.0;
+            double _celsius = 0.0;
+            return _celsius; 
+        }
+    }
+}
