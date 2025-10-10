541e68c84dfb2718fb9df19d0a4a48ed3774011c
Ellie Thomas <elliethom294@gmail.com>
2025-10-10 15:21:37 +0100

    Adding more methods

--- UtilityKnife/UtilityKnife/Calculator/Calculator.cs
+++ UtilityKnife/UtilityKnife/Calculator/Calculator.cs
@@ -8,9 +8,29 @@ namespace UtilityKnife.Calculator
 {
     public class Calculator
     {
-        public static int Add (int x, int y)
+        public static int Add (double x, double y)
         {
             return x + y;
+        }
+
+        public static int Subtract(double x, double y)
+        {
+            return x - y;
+        }
+
+        public static int Multiply(double x, double y)
+        {
+            return x * y;
+        }
+
+        public static int Division(int x, int y)
+        {
+            return x / y;
+        }
+
+        public static int Add(double x, double y)
+        {
+            return x / y;
         }
     }
 }
